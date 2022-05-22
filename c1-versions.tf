1e9bc19cdfd379a63c0972982199530a54dc7f7b
Karthik M L <94063813+KarthikML08071991@users.noreply.github.com>
2022-05-22 06:45:16 +0530

    Terraform Block and VPC Creation
    Creating a VPC with just cidr_block and adding versions and required providers in terraform block.

--- c1-versions.tf
+++ c1-versions.tf
@@ -1,25 +1,22 @@
 # Terraform Block
 terraform {
-  required_version = "~> 0.14.6"
+  required_version = ">= 1.0.0"
   required_providers {
-    aws = {
+    aws= {
       source = "hashicorp/aws"
-      version = "~> 3.0"
-    }
+      version = ">= 0.0"    
   }
 }
+}
 
-# Provider-1 for us-east-1 (Default Provider)
-provider "aws" {
-  region = "us-east-1"
+# Provider Block
+
+provider "aws"{
+  region = "ap-south-1"
   profile = "default"
 }
 
-# Provider-2 for us-west-1
-provider "aws" {
-  region = "us-west-1"
-  profile = "default"
-  alias = "aws-west-1"
-}
-
-
+/*
+Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
+$HOME/.aws/credentials
+*/
